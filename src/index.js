7fa05c4749a803d0d6c9b8a1f8da4d6e0463fede
wangsf <wangsf@shinemo.com>
2022-04-26 15:35:06 +0800

    feat: add createElement& render

--- src/index.js
+++ src/index.js
@@ -1,36 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
-// 18v
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// ==============================
-
-// const element = <h1 title="foo">Hello</h1>;
-// const element = React.createElement('h1', {
-//   title: 'foo'
-// }, 'Hello')
+/** @jsxRuntime classic */
+// 需加此句，否则默认为automatic，jsx不会加载我们的代码
+// import React from 'react';
 const container = document.getElementById("root");
 
-// 0. test
-// const element = {
-//   type: 'h1',
-//   props: {
-//     title: 'foo',
-//     children: 'Hello'
-//   }
-// }
-// const node = document.createElement(element.type);
-// node["title"] = element.props.title;
+function createElement (type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child => {
+        return typeof child === 'object' ? child : createTextElement(child);
+      })
+    }
+  }
+}
+function createTextElement (text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: [],
+    }
+  }
+}
 
-// const text = document.createTextNode('');
-// text["nodeValue"] = element.props.children;
+function render (element, container) {
+  const dom = 
+    element.type === 'TEXT_ELEMENT'
+      ? document.createTextNode('')
+      : document.createElement(element.type);
 
-// node.appendChild(text);
-// container.appendChild(node);
+  const isProperty = key => key !== 'children';
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach(propName => {
+      dom[propName] = element.props[propName]
+    })
+  
+  element.props.children.forEach(child => {
+    render(child, dom);
+  })
+
+  container.appendChild(dom);
+}
+
+const Fact = {
+  createElement,
+  render
+}
+
+/** @jsx Fact.createElement */
+const element = (
+  <div style="background: salmon;">
+    <h1>Hello World</h1>
+    <h2 style="text-align: right;">from Fact</h2>
+  </div>
+)
+
+Fact.render(element, container)

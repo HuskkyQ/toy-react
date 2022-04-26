d4429ed16c6a7bf5955f0c2688c408f05c945452
wangsf <wangsf@shinemo.com>
2022-04-26 15:03:38 +0800

    feat: add v0.0.0

--- src/index.js
+++ src/index.js
@@ -19,18 +19,18 @@ import './index.css';
 const container = document.getElementById("root");
 
 // 0. test
-// const element = {
-//   type: 'h1',
-//   props: {
-//     title: 'foo',
-//     children: 'Hello'
-//   }
-// }
-// const node = document.createElement(element.type);
-// node["title"] = element.props.title;
+const element = {
+  type: 'h1',
+  props: {
+    title: 'foo',
+    children: 'Hello'
+  }
+}
+const node = document.createElement(element.type);
+node["title"] = element.props.title;
 
-// const text = document.createTextNode('');
-// text["nodeValue"] = element.props.children;
+const text = document.createTextNode('');
+text["nodeValue"] = element.props.children;
 
-// node.appendChild(text);
-// container.appendChild(node);
+node.appendChild(text);
+container.appendChild(node);

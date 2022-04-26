8ed6ceb83b79b9f62f5bb61a9fe3d493554bad80
wangsf <wangsf@shinemo.com>
2022-04-26 16:09:48 +0800

    feat: complete performUnitOfWork

--- src/index.js
+++ src/index.js
@@ -24,24 +24,29 @@ function createTextElement (text) {
   }
 }
 
-function render (element, container) {
+function createDom (fiber) {
   const dom = 
-    element.type === 'TEXT_ELEMENT'
-      ? document.createTextNode('')
-      : document.createElement(element.type);
+    fiber.type === 'TEXT_ELEMENT'
+        ? document.createTextNode('')
+        : document.createElement(fiber.type);
 
   const isProperty = key => key !== 'children';
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProperty)
     .forEach(propName => {
-      dom[propName] = element.props[propName]
+      dom[propName] = fiber.props[propName]
     })
-  
-  element.props.children.forEach(child => {
-    render(child, dom);
-  })
 
-  container.appendChild(dom);
+  return dom;
+}
+
+function render (element, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    }
+  }
 }
 
 let nextUnitOfWork = null;
@@ -62,13 +67,59 @@ function workLoop (deadline) {
 
 requestIdleCallback(workLoop);
 
-function performUnitOfWork (nextUnitOfWork) {
-  // TODO
+function performUnitOfWork (fiber) {
   /**
    * To start using the loop we’ll need to set the first unit of work,
    *  and then write a performUnitOfWork function that
    *  not only performs the work but also returns the next unit of work.
    */
+  // add dom node
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+  // create new fibers
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+  while (index < elements.length) {
+    // 取出一个子节点
+    const element = elements[index];
+    // 生成一个fiber
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    }
+    // child指向第一个子节点
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      // 若存在多个子节点，将不是第一个子节点的节点挂载到前一个子节点的sibling上
+      prevSibling.sibling = newFiber;
+    }
+    // 设置上一个子节点
+    prevSibling = newFiber;
+    index++;
+  }
+  // return next unit of work
+  if (fiber.child) {
+    // 存在子节点，返回其
+    return fiber.child;
+  }
+  // 如果没有子节点
+  let nextFiber = fiber;
+  while (nextFiber) {
+    // 如果有 兄弟节点 直接返回它
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    // 若无兄弟节点，向上查找其父亲（后续查找是否有父亲的兄弟节点（叔叔节点））
+    nextFiber = nextFiber.parent;
+  }
 }
 
 
